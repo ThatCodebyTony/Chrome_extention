03421a35a7bc877d8f28cbd94fbf3b72000d53e7
ThatCodebyTony <twu1415@gmail.com>
2024-11-05 13:37:51 -0800

    added array and inputEl variables

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let saveInputEl = document.getElementById('input-btn');
+let myLeads = []
+const inputEl = document.getElementById("input-el");
+const saveInputEl = document.getElementById('input-btn');
 
 saveInputEl.addEventListener("click", function() {
     saveInputEl.textContent = "Button Clicked!";
