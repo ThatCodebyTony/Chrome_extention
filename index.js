a95152f71a2847b0a81f76c2316cd5e68c681b65
ThatCodebyTony <twu1415@gmail.com>
2024-11-05 13:52:56 -0800

    implemented innerHtml forloop function

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let myLeads = []
+let myLeads = ["hi", "hello", "hey"];
 const inputEl = document.getElementById("input-el");
 const saveInputEl = document.getElementById('input-btn');
+const ulEl = document.getElementById("ul-el");
 
 saveInputEl.addEventListener("click", function() {
-    saveInputEl.textContent = "Button Clicked!";
-    console.log("Button Clicked!");
+    // saveInputEl.textContent = "Button Clicked!";
+    // console.log("Button Clicked!");
+    myLeads.push(inputEl.value);
 })
+
+for (let item of myLeads) {
+    ulEl.innerHTML += "<li>" + item + "</li>";
+}
