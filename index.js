9f24b6d752979d6fdd1f2bdb85dff6cadce28346
ThatCodebyTony <twu1415@gmail.com>
2024-11-05 13:32:18 -0800

    Trying out AddEventListener

---
+++ index.js
@@ -0,0 +1,6 @@
+let saveInputEl = document.getElementById('input-btn');
+
+saveInputEl.addEventListener("click", function() {
+    saveInputEl.textContent = "Button Clicked!";
+    console.log("Button Clicked!");
+})
