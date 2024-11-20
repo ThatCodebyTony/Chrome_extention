4d5de1cefc398a8d21c8858bd5438b949d68dc9a
ThatCodebyTony <twu1415@gmail.com>
2024-11-19 22:41:37 -0800

    added buy button via inner text

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let myLeads = ["hi", "hello", "hey"];
 const inputEl = document.getElementById("input-el");
 const saveInputEl = document.getElementById('input-btn');
 const ulEl = document.getElementById("ul-el");
+const container = document.getElementById("container");
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy(){
+    container.innerHTML = "<p>Thank you for buying!</p>"
+}
 
 saveInputEl.addEventListener("click", function() {
     // saveInputEl.textContent = "Button Clicked!";
@@ -14,6 +21,7 @@ for (let item of myLeads) {
     
 
 }
+
 // container.innerHTML = "<button>Buy!</button>"
 // container.addEventListener("click", function(){
 //     container.innerHTML += "<li>Thank you for buying!</li>"
