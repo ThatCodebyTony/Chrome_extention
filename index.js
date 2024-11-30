6ec2dda718e59b09683b1cf341d7032c4f8fad69
ThatCodebyTony <twu1415@gmail.com>
2024-11-29 17:31:09 -0800

    Testing out promise.race

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
+// Create three promises that resolve after different times
 const promise1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('First promise resolved');
+    }, 1000);
+});
+
+const promise2 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Second promise resolved');
+    }, 2000);
+});
+
+const promise3 = new Promise((resolve) => {
     setTimeout(() => {
         resolve('Third promise resolved');
     }, 3000);
 });
 
-const promise2 = new Promise((resolve)=> {
-    setTimeout(() => {
-        resolve("Promise 2 resolved!");
-    },1000);
-});
-
-const promise3 = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve("Promise 3 resolved!");
-    },1000);
-})
-
-Promise.all([promise1,promise2,promise3])
-    .then((messages) => {
-        console.log('All promises resolved:');
-        messages.forEach((messages, index) => {
-            console.log(`Promise ${index+1}: ${messages}`);
-        });
+// Use Promise.all to run the promises in parallel
+Promise.race([promise1,promise2,promise3])
+    .then((message) => {
+        console.log("First resolved Promise: ", message);
     })
-    .catch((error) => {
-        console.error("One of the promises rejected:",error)
-    });
-
-    
+    .catch((error)=> {
+        console.error('Promise rejected:', error);
+    })
