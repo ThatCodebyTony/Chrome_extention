c2df4451d4e50e5f785564b5a1d2088906c497f0
ThatCodebyTony <twu1415@gmail.com>
2024-11-29 18:49:55 -0800

    async/await

--- index.js
+++ index.js
@@ -8,21 +8,18 @@ function delayPromise(message, delay) {
     });
 }
 
-function setDelay() {
-    return delayPromise("First Promise",1000)
-        .then((result)=> {
-            console.log(result);
-            return delayPromise("Second Promise",2000)
-        })
-        .then((result)=> {
-            console.log(result);
-            return delayPromise("Third Promise",3000)
-        })
-        .then((result)=> {
-            console.log(result);
-        })
-        .catch((error)=> {
-            console.error("Error:".error)
-        })
-}
+async function executeAsync() {
+    try {
+        const result1 = await delayPromise("First Promise done", 1000);
+        console.log(result1);
+        
+        const result2 = await delayPromise("Second Prmise done", 2000);
+        console.log(result2)
 
+        const result3 = await delayPromise("Third Promise done", 3000);
+        console.log(result3)
+    } catch(error) {
+        console.error("Error",error);
+    }
+}
+executeAsync();
