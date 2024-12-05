8d8854e63074a35597a0f4d6552704c6abfbdcbd
ThatCodebyTony <twu1415@gmail.com>
2024-12-05 13:09:34 -0800

    Updated remove child and parent DOM

--- index.js
+++ index.js
@@ -53,33 +53,35 @@ function fetchData() {
   }
   
 
-const parent = document.body.querySelector(".parent");
-const addChild = document.body.querySelector("#add-child");
-addChild.addEventListener("click", () => {
-  // Only add a child if we don't already have one
-  // in addition to the text node "parent"
-  if (parent.childNodes.length > 1) {
-    return;
-  }
-  const child = document.createElement("div");
-  child.classList.add("child");
-  child.textContent = "child";
-  parent.appendChild(child);
+  const parent = document.body.querySelector(".parent");
+  const addChild = document.body.querySelector("#add-child");
+  const container = document.body.querySelector(".container")
+  addChild.addEventListener("click", () => {
+    // Only add a child if we don't already have one
+    // in addition to the text node "parent"
+    if (parent.childNodes.length > 1) {
+      return;
+    }
+    const child = document.createElement("div");
+    child.classList.add("child");
+    child.textContent = "child";
+    parent.appendChild(child);
+    
+    // Create a new parent div and add it to the body
+    const newParent = document.createElement("div");
+    newParent.classList.add("parent");
+    newParent.textContent = "New Parent";
+    document.body.appendChild(newParent);
+  });
   
-  // Create a new parent div and add it to the body
-  const newParent = document.createElement("div");
-  newParent.classList.add("parent");
-  newParent.textContent = "New Parent";
-  document.body.appendChild(newParent);
-});
-
-const removeChild = document.body.querySelector("#remove-child");
-removeChild.addEventListener("click", () => {
-  const child = document.body.querySelector(".child");
-  parent.removeChild(child);
-  const firstParent = document.body.querySelector(".parent");
-  document.body.removeChild(firstParent);
+  const removeChild = document.body.querySelector("#remove-child");
+  removeChild.addEventListener("click", () => {
+    const child = document.body.querySelector(".child");
+    parent.removeChild(child);
+    const firstParent = document.body.querySelector(".parent");
+    container.removeChild(firstParent);
+    
+  });
   
-});
-
-
+  
+  
